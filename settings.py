bcd98abd4bb399d7027fbd9a9f8eb7e98dae346a
Syam Chand Banisetti <syamchandbanisetti@gmail.com>
2025-06-06 23:16:51 +0530

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,38 @@
+# settings.py
+from pathlib import Path
+import sys
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parent
+if ROOT not in sys.path:
+    sys.path.append(str(ROOT))
+ROOT = ROOT.relative_to(Path.cwd())
+
+# Sources
+IMAGE = 'Image'
+VIDEO = 'Video'
+WEBCAM = 'Webcam'
+RTSP = 'RTSP'
+YOUTUBE = 'YouTube'
+SOURCES_LIST = [IMAGE, VIDEO, WEBCAM, RTSP, YOUTUBE]
+
+# Images
+IMAGES_DIR = ROOT / 'images'
+DEFAULT_IMAGE = IMAGES_DIR / 'office_4.jpg'
+DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
+
+# Videos
+VIDEO_DIR = ROOT / 'videos'
+VIDEOS_DICT = {
+    'video_1': VIDEO_DIR / 'video_1.mp4',
+    'video_2': VIDEO_DIR / 'video_2.mp4',
+    'video_3': VIDEO_DIR / 'video_3.mp4',
+}
+
+# Models
+MODEL_DIR = ROOT / 'weights'
+DETECTION_MODEL = MODEL_DIR / 'yolov8n.pt'
+SEGMENTATION_MODEL = MODEL_DIR / 'yolov8n-seg.pt'
+
+# Webcam
+WEBCAM_PATH = 0
