4c544339f3389c379a64fb30d71d709f4c5a406d
Syam Chand Banisetti <syamchandbanisetti@gmail.com>
2025-06-07 11:42:15 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,3 @@
-# settings.py
 from pathlib import Path
 import sys
 
@@ -16,12 +15,11 @@ RTSP = 'RTSP'
 YOUTUBE = 'YouTube'
 SOURCES_LIST = [IMAGE, VIDEO, WEBCAM, RTSP, YOUTUBE]
 
-# Images
+# Paths
 IMAGES_DIR = ROOT / 'images'
 DEFAULT_IMAGE = IMAGES_DIR / 'office_4.jpg'
 DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
 
-# Videos
 VIDEO_DIR = ROOT / 'videos'
 VIDEOS_DICT = {
     'video_1': VIDEO_DIR / 'video_1.mp4',
@@ -29,10 +27,8 @@ VIDEOS_DICT = {
     'video_3': VIDEO_DIR / 'video_3.mp4',
 }
 
-# Models
 MODEL_DIR = ROOT / 'weights'
 DETECTION_MODEL = MODEL_DIR / 'yolov8n.pt'
 SEGMENTATION_MODEL = MODEL_DIR / 'yolov8n-seg.pt'
 
-# Webcam
 WEBCAM_PATH = 0
